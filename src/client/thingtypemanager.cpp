6f7bd05dbc04bad24e9f97c8c522b31bf69d8d9b
kokekanon <114332266+kokekanon@users.noreply.github.com>
2026-01-31 00:12:03 -0300

    feat: Implement conditional Protobuf compilation (FRAMEWORK_PROTOBUF)

--- src/client/thingtypemanager.cpp
+++ src/client/thingtypemanager.cpp
@@ -31,7 +31,9 @@
 #include "framework/core/filestream.h"
 #include "framework/core/resourcemanager.h"
 #include "framework/otml/otmldocument.h"
+#ifdef FRAMEWORK_PROTOBUF
 #include <staticdata.pb.h>
+#endif
 
 #ifdef FRAMEWORK_EDITOR
 #include "itemtype.h"
@@ -144,6 +146,7 @@ bool ThingTypeManager::loadOtml(std::string file)
 
 bool ThingTypeManager::loadAppearances(const std::string& file)
 {
+#ifdef FRAMEWORK_PROTOBUF
     try {
         if (!g_game.getFeature(Otc::GameLoadSprInsteadProtobuf)) {
             g_spriteAppearances.unload();
@@ -222,8 +225,13 @@ bool ThingTypeManager::loadAppearances(const std::string& file)
         g_logger.error("Failed to load '{}' (Appearances): {}", file, e.what());
         return false;
     }
+#else
+    g_logger.error("Protobuf not supported in this build. enable FRAMEWORK_PROTOBUF");
+    return false;
+#endif
 }
 
+#ifdef FRAMEWORK_PROTOBUF
 namespace {
     using RaceBank = google::protobuf::RepeatedPtrField<staticdata::Creature>;
 
@@ -299,6 +307,13 @@ bool ThingTypeManager::loadStaticData(const std::string& file)
 
     return false;
 }
+#else
+bool ThingTypeManager::loadStaticData(const std::string& file)
+{
+    g_logger.error("Protobuf not supported in this build. enable FRAMEWORK_PROTOBUF");
+    return false;
+}
+#endif
 
 const ThingTypeList& ThingTypeManager::getThingTypes(const ThingCategory category)
 {
