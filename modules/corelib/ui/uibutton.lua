3c31217295a353c9b1dcdd4ccb40972c2f3e3168
LIBERGOD <37589902+libergod@users.noreply.github.com>
2026-02-24 11:06:19 -0800

    Fix bug with pointer cursors

--- modules/corelib/ui/uibutton.lua
+++ modules/corelib/ui/uibutton.lua
@@ -4,11 +4,24 @@ UIButton = extends(UIWidget, 'UIButton')
 function UIButton.create()
     local button = UIButton.internalCreate()
     button:setFocusable(false)
+    button.cursorPushed = false
     return button
 end
 
 function UIButton:onMouseRelease(pos, button)
     return self:isPressed()
+end
+
+function UIButton:onDestroy()
+    -- Clean up cursor if it was pushed
+    if self.cursorPushed then
+        if modules.client_options and modules.client_options.getOption('nativeCursor') then
+            g_window.restoreMouseCursor()
+        else
+            g_mouse.popCursor('pointerbutton')
+        end
+        self.cursorPushed = false
+    end
 end
 
 function UIButton:onHoverChange(hovered)
@@ -19,17 +32,33 @@ function UIButton:onHoverChange(hovered)
     local nativeCursor = modules.client_options.getOption('nativeCursor')
     local animatedCursor = modules.client_options.getOption('showAnimatedCursor')
     
-    -- Native cursor takes priority - don't change cursor when using native Windows cursor
-    if nativeCursor then
-        return
-    end
-    
     -- Animated cursor mode - show pointer button on hover
-    if animatedCursor then
+    if animatedCursor and not nativeCursor then
         if hovered then
-            g_mouse.pushCursor('pointerbutton')
+            -- Only push if we haven't already
+            if not self.cursorPushed then
+                g_mouse.pushCursor('pointerbutton')
+                self.cursorPushed = true
+            end
         else
-            g_mouse.popCursor('pointerbutton')
+            -- Only pop if we pushed
+            if self.cursorPushed then
+                g_mouse.popCursor('pointerbutton')
+                self.cursorPushed = false
+            end
+        end
+    elseif nativeCursor then
+        -- Native cursor mode - use hand pointer
+        if hovered then
+            if not self.cursorPushed then
+                g_window.setSystemCursor('hand')
+                self.cursorPushed = true
+            end
+        else
+            if self.cursorPushed then
+                g_window.restoreMouseCursor()
+                self.cursorPushed = false
+            end
         end
     end
     -- When both are disabled, use default Tibia cursors (no animation)
