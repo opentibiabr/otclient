b9c68ef8a2b27c4869a159381c424e61a4d4e205
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2026-05-29 12:56:32 -0300

    chore: log startup timestamp at main entry

--- init.lua
+++ init.lua
@@ -105,7 +105,6 @@ end
 
 -- setup logger
 g_logger.setLogFile(g_resources.getWorkDir() .. g_app.getCompactName() .. '.log')
-g_logger.info(os.date('application started at %b %d %Y %X'))
 g_logger.info("operating system: " .. g_platform.getOSName())
 
 -- print first terminal message
